1b92b79dad9dc341769abc20e22f0362229e866e
Hoy <junghoyo92@gmail.com>
2016-01-29 13:43:23 -0800

    initial

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var numberText: UITextField!
+    
+    @IBOutlet var resultText: UILabel!
+    
+    @IBAction func isPrime(sender: AnyObject) {
+        
+        if let number = Int(numberText.text!) {
+            var isPrime = true
+        
+            if number == 1 {
+                isPrime = false
+            }
+        
+            if number != 2 && number != 1 {
+            
+                for var i=2; i<number; i++ {
+                    if number%i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+        
+            if isPrime {
+                resultText.text = "\(number) IS Prime!"
+            } else {
+                resultText.text = "\(number) is NOT Prime!"
+            }
+        } else {
+            resultText.text = "Please Enter a Whole Number"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.numberText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Closes the keyboard by pressing anywhere
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    // Closes the keyboard by pressing return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
 }
 
